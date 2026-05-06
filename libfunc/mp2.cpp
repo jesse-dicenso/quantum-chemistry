edc1ab9970fe5abe3898bae32df7c0e030c5d60a
Jesse DiCenso <jessedicenso@Jesses-MacBook-Pro-4.local>
2026-05-06 10:38:35 -0700

    mp24

--- libfunc/mp2.cpp
+++ libfunc/mp2.cpp
@@ -13,7 +13,7 @@ struct tens4 {
         return m_data[i*m_strides[0] + j*m_strides[1] + k*m_strides[2] + l];
     }
     double& operator()(size_t i, size_t j, size_t k, size_t l) {
-        assert((i < d1) && (j < d2) && (k < d3) && (l < d4));
+        assert((i < m_dims[0]) && (j < m_dims[1]) && (k < m_dims[2]) && (l < m_dims[3]));
         return m_data[i*m_strides[0] + j*m_strides[1] + k*m_strides[2] + l];
     }
 }
@@ -37,42 +37,54 @@ double MP2_ENERGY(
 
     // Transform first index (occ)
     tens4 T1(nocc, K, K, K);
-    for (size_t i = 0; i < nocc; ++i) // MO (occ)
-    for (size_t mu = 0; mu < K; ++mu) // AO
-    for (size_t nu = 0; nu < K; ++nu) // AO
-    for (size_t ld = 0; ld < K; ++ld) // AO
-    for (size_t sg = 0; sg < K; ++sg) // AO
-        T1(i, nu, ld, sg) += C(mu, i) * eris[mu][nu][ld][sg]; 
+    for (size_t i = 0; i < nocc; ++i) 
+    for (size_t nu = 0; nu < K; ++nu) 
+    for (size_t ld = 0; ld < K; ++ld)
+    for (size_t sg = 0; sg < K; ++sg) {
+        double t1 = 0.0;
+        for (size_t mu = 0; mu < K; ++mu) {
+            t1 += C(mu, i) * eris[mu][nu][ld][sg];
+        }
+        T1(i, nu, ld, sg) = t1;
     }
 
     // Transform second index (virt)
     tens4 T2(nocc, nvirt, K, K);
-    for (size_t i = 0; i < nocc ; ++i) // MO (occ)
-    for (size_t a = 0; a < nvirt; ++a) // MO (virt)
-    for (size_t nu = 0; nu < K; ++nu)  // AO
-    for (size_t ld = 0; ld < K; ++ld)  // AO
-    for (size_t sg = 0; sg < K; ++sg)  // AO
-        T2(i, a, ld, sg) += C(nu, a+nocc) * T1(i, nu, ld, sg); 
+    for (size_t i = 0; i < nocc ; ++i)
+    for (size_t a = 0; a < nvirt; ++a)
+    for (size_t ld = 0; ld < K; ++ld)
+    for (size_t sg = 0; sg < K; ++sg) {
+        double t2 = 0.0;
+        for (size_t nu = 0; nu < K; ++nu) {
+            t2 += C(nu, a+nocc) * T1(i, nu, ld, sg); 
+        }
+        T2(i, a, ld, sg) = t2;
     }
 
     // Transform third index (occ)
     tens4 T3(nocc, nvirt, nocc, K);
-    for (size_t i = 0; i < nocc ; ++i) // MO (occ)
-    for (size_t a = 0; a < nvirt; ++a) // MO (virt)
-    for (size_t j = 0; j < nocc ; ++j) // MO (occ)
-    for (size_t ld = 0; ld < K; ++ld)  // AO
-    for (size_t sg = 0; sg < K; ++sg)  // AO
-        T3(i, a, j, sg) += C(ld, j) * T2(i, a, ld, sg); 
+    for (size_t i = 0; i < nocc ; ++i)
+    for (size_t a = 0; a < nvirt; ++a)
+    for (size_t j = 0; j < nocc ; ++j)
+    for (size_t sg = 0; sg < K; ++sg) {
+        double t3 = 0.0;
+        for (size_t ld = 0; ld < K; ++ld) {
+            t3 += C(ld, j) * T2(i, a, ld, sg); 
+        }
+        T3(i, a, j, sg) = t3;
     }
 
     // Transform fourth index (virt)
     tens4 MO_ERIS(nocc, nvirt, nocc, nvirt);
-    for (size_t i = 0; i < nocc ; ++i) // MO (occ)
-    for (size_t a = 0; a < nvirt; ++a) // MO (virt)
-    for (size_t j = 0; j < nocc ; ++j) // MO (occ)
-    for (size_t b = 0; b < nvirt; ++b) // MO (virt)
-    for (size_t sg = 0; sg < K; ++sg)  // AO
-        MO_ERIS(i, a, j, b) += C(sg, b+nocc) * T3(i, a, j, sg); 
+    for (size_t i = 0; i < nocc ; ++i)
+    for (size_t a = 0; a < nvirt; ++a)
+    for (size_t j = 0; j < nocc ; ++j)
+    for (size_t b = 0; b < nvirt; ++b) {
+        double mo_eris = 0.0;
+        for (size_t sg = 0; sg < K; ++sg) {
+            mo_eris += C(sg, b+nocc) * T3(i, a, j, sg);
+        }
+       MO_ERIS(i, a, j, b) = mo_eris; 
     }
 
     //
@@ -83,8 +95,10 @@ double MP2_ENERGY(
     for (size_t a = 0; a < nvirt; ++a)   // MO (virt)
     for (size_t j = 0; j < nocc ; ++j)   // MO (occ)
     for (size_t b = 0; b < nvirt; ++b) { // MO (virt)
-        const double Delta_ijab = e(i,i) + e(j,j) - e(a,a) - e(b,b);
-        const double numerator  = MO_ERIS(i,a,j,b) * (2.0 * MO_ERIS(i,a,j,b) - MO_ERIS(i,b,j,a));
+        const size_t A = a + nocc;
+        const size_t B = b + nocc;
+        const double Delta_ijab = e(i,i) + e(j,j) - e(A,A) - e(B,B);
+        const double numerator  = MO_ERIS(i,a,j,b) * (2.0 * MO_ERIS(i,a,j,b) - MO_ERIS(i,b,j,a)); 
         E_MP2 += numerator / Delta_ijab;
     }
 
